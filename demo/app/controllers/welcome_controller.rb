5765a0c18d303d32a71cd21e25ff79e1de42e446
Win <oracledesigntw@gmail.com>
2015-06-11 12:11:12 +0800

    update welcome

--- demo/app/controllers/welcome_controller.rb
+++ demo/app/controllers/welcome_controller.rb
@@ -1,7 +1,11 @@
 class WelcomeController < ApplicationController
 
+    def index
+        
+    end
+
     #GET /welcome/say_hello
-  def say
+    def say
  
     #views/welcome/say.html.erb
   end
